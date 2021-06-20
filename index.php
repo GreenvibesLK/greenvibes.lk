3ce1b10a84385a1e4989f534993878d0c80efb58
Srimal Fernando <dilankasrimal23@gmail.com>
2021-06-20 13:57:02 +0530

    test index.php
    index.php test with xamp

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php require_once('inc/connection.php'); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+
 </head>
 <body>
-	
+	Hello World
 </body>
 </html>
 
-<?php mysqli_close($connection) ?>
+<?php mysqli_close($connection); ?>
