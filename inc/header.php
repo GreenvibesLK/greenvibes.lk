570abfbfade688ff50087a10ca634a3f2f0001a5
Srimal Fernando <dilankasrimal23@gmail.com>
2021-06-22 16:41:12 +0530

    Header css propities

--- inc/header.php
+++ inc/header.php
@@ -3,8 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>MY MEAL-GreenVibes</title>
-    <link rel="stylesheet" href="css/style.css">
+	<title>GreenVibes</title>
+    <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" >
@@ -12,7 +12,7 @@
 <body>
     <section class="header">
         <nav>
-            <a href="index.html" onclick="hideMenu()"><img src="images/GreenvibesLK white.png">  </a>
+            <a href="index.php" onclick="hideMenu()"><img src="img/GreenvibesLK white.png">  </a>
             <div class="nav-links" id="navLinks">
                 <i class="fa fa-times" onclick="hideMenu()"></i>
                 <ul>
