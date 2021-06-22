109bccbb6d89a0b05d05bc9e16a316cffb1d94e1
Srimal Fernando <dilankasrimal23@gmail.com>
2021-06-22 16:45:37 +0530

    Body edititng

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!-- <?php require_once('inc/header.php'); ?> -->
+<?php require_once('inc/header.php'); ?>
 
 
 <!DOCTYPE html>
@@ -8,6 +8,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Greenvibes</title>
     <link rel="stylesheet" href="css/style.css">
+
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" >
