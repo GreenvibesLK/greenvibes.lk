612634b4b22d138f6500e7607cba323c30ef5ef6
Srimal Fernando <dilankasrimal23@gmail.com>
2021-06-22 15:50:10 +0530

    Add header css and images, log

--- inc/header.php
+++ inc/header.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>MY MEAL-GreenVibes</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" >
@@ -29,7 +29,6 @@
         <p>ShareTheMeal is a crowdfunding smartphone application to fight global hunger through the United Nations World Food Programme (WFP).</p>
         <a href="" class="visit-btn">Visit us</a>
         </div>
-        
     </section>
 
 
