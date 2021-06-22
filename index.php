5bdc62af97a4c06f7438e42a4b1b5511e1e37a57
Srimal Fernando <dilankasrimal23@gmail.com>
2021-06-22 17:07:46 +0530

    edit hover byn

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php require_once('inc/header.php'); ?>
 
+
     <div class="text-box">
         <h1>Make it simple. But significant</h1>
         <p>ShareTheMeal is a crowdfunding smartphone application to fight global hunger through the United Nations World Food Programme (WFP).</p>
         <a href="" class="visit-btn">Visit us</a>
         </div>
     </section>
-    
+
 <body>
 
 
