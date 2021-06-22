6d209e2a7e226a4a05a21b768cacc91c231f8b51
Srimal Fernando <dilankasrimal23@gmail.com>
2021-06-22 14:47:11 +0530

    Update header

--- inc/header.php
+++ inc/header.php
@@ -3,6 +3,37 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>MY MEAL-GreenVibes</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" >
+    </head>
+<body>
+    <section class="header">
+        <nav>
+            <a href="index.html" onclick="hideMenu()"><img src="images/GreenvibesLK white.png">  </a>
+            <div class="nav-links" id="navLinks">
+                <i class="fa fa-times" onclick="hideMenu()"></i>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">Contact</a></li> 
+                </ul>
+            </div>
+            <i class="fa fa-bars" onclick="showMenu()"></i>  
+        </nav> 
+    <div class="text-box">
+        <h1>Make it simple. But significant</h1>
+        <p>ShareTheMeal is a crowdfunding smartphone application to fight global hunger through the United Nations World Food Programme (WFP).</p>
+        <a href="" class="visit-btn">Visit us</a>
+        </div>
+        
+    </section>
+
+
+
 	<title>Greenvibes</title>
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="shortcut icon" type="image/svg" href="img/gv logo favicon green.svg">
