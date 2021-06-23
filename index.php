7214600625d8ff4867837ab570ae879af018ea7e
Srimal Fernando <dilankasrimal23@gmail.com>
2021-06-23 17:49:27 +0530

    header img fixed

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<style type=text/css> .header {
+    background-image: linear-gradient(rgba(4,9,30,0.7),rgba(4,9,30,0.7)),url(img/contact.gif);}
+</style>
+
 <?php $title = 'Home - '; ?>
 <?php require_once('inc/header.php'); ?>
   <div class="text-box">
@@ -17,7 +21,6 @@
 
 
 
-
 </body>
 </html>
 <?php require_once('inc/footer.php'); ?>
@@ -26,14 +29,4 @@
 
 
 
-<!-- page styles -->
- <style type=text/css>
-   .header {
-    min-height: 100vh;
-    width: 100%;
-    background-image: linear-gradient(rgba(4,9,30,0.7),rgba(4,9,30,0.7)),url(img/contact.gif);
-    background-position: center;
-    background-size: cover;
-    position: relative;
- }
-</style>
+
