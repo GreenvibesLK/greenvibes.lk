1b5329aa1fd2f8273db9b2ee9f0e3a482831b6a3
Srimal Fernando <dilankasrimal23@gmail.com>
2021-06-23 17:56:30 +0530

    Nav dropped and fixed header img error
    this is about website header and content pages.  I cloned about and contact pages from index page

--- about-us.php
+++ about-us.php
@@ -1,11 +1,11 @@
 <style type=text/css> .header {
-    background-image: linear-gradient(rgba(4,9,30,0.7),rgba(4,9,30,0.7)),url(img/home.jpg);}
+    background-image: linear-gradient(rgba(4,9,30,0.7),rgba(4,9,30,0.7)),url(img/about.gif);}
 </style>
 
-<?php $title = 'Home - '; ?>
+<?php $title = 'About Us - '; ?>
 <?php require_once('inc/header.php'); ?>
   <div class="text-box">
-        <h1>Make it simple. But significant</h1>
+        <h1>Who We Are ?</h1>
         <p>ShareTheMeal is a crowdfunding smartphone application to fight global hunger through the United Nations World Food Programme (WFP).</p>
         <a href="" class="visit-btn">Visit us</a>
         </div>
