eaff9ed241677252115641ba09f222c2a8049a88
Srimal Fernando <dilankasrimal23@gmail.com>
2021-06-26 23:37:18 +0530

    Add tags to content

--- about-us.php
+++ about-us.php
@@ -12,25 +12,26 @@
     </section>
 <body>
 
-About Greenvibes Technologies
-We are Greenvibes Technologies. A team with a diversified and forward thinking capabilities in the field of software with the elements of creating unique solutions.
+<h2>About Greenvibes Technologies</h2>
+<p>We are Greenvibes Technologies. A team with a diversified and forward thinking capabilities in the field of software with the elements of creating unique solutions.
 
 Just as machines have extended man's mechanical power and his convenience and comfort, Information Technology as commonly pictured by computers, 
 is extending man's mind or brain or intellectual power. 
 This paved the way for us to think out of the box and come up with systems spending our time and effort targeting areas that are untouched.
 
-advancing knowledge through research, and disseminating knowledge by means of the internet, researching and writing books, producing newsletters, editorials, monographs, seminars, lectures, conferences and the like.
+advancing knowledge through research, and disseminating knowledge by means of the internet, researching and writing books, producing newsletters, editorials, monographs, seminars, lectures, conferences and the like.</p>
 
 
-Our vission-
-Our Vision is to empower entrepreneurs with a roadmap and support that guides them through the transition from start-ups, to micro-enterprises, 
-and on to small and medium-sized businesses that create jobs, lead innovation, and increase economic development in their local communities.
+<h2>Our Vision</h2>
+<p>Our Vision is to empower entrepreneurs with a roadmap and support that guides them through the transition from start-ups, to micro-enterprises, 
+and on to small and medium-sized businesses that create jobs, lead innovation, and increase economic development in their local communities.</p>
 
-Our mission-
-Incorporating forward thinking with imagination, creativity, and originality to reach our goal to become a leader in software solutions.
+<h2>Our Mission</h2>
+<p>Incorporating forward thinking with imagination, creativity, and originality to reach our goal to become a leader in software solutions.
+</p>
 
-Our services-
-Have you hit a dead end with brainstorming marketing ideas for your technology company? 
+<h2>Our Services</h2>
+<p>Have you hit a dead end with brainstorming marketing ideas for your technology company? 
 Are your current technology product marketing strategies falling short of your goals? Do you need some help improving your technology marketing plan?
 
 Then, keep reading! This article shares the best marketing strategies for IT companies and tech organizations so you can focus your time, energy, 
@@ -54,7 +55,7 @@ To take the brand to the next level, a business should apply diverse marketing s
 Traditional methods focused on television and radio commercials as well as newspaper ads or simply putting marketing material in a mailbox and hoping for the best do not work anymore.
 
 Though advertising via television can still have some impact, there are numerous digital marketing strategies that can have a more direct and closer influence on the public.
-
+</p>
 
 </body>
 </html>
