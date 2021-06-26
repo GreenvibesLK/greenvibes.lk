8fbafb84923226f3184b5ccea9f8c38be39a2ff8
Srimal Fernando <dilankasrimal23@gmail.com>
2021-06-26 23:53:38 +0530

    Edit content in our services

--- about-us.php
+++ about-us.php
@@ -30,33 +30,6 @@ and on to small and medium-sized businesses that create jobs, lead innovation, a
 <p>Incorporating forward thinking with imagination, creativity, and originality to reach our goal to become a leader in software solutions.
 </p>
 
-<h2>Our Services</h2>
-<p>Have you hit a dead end with brainstorming marketing ideas for your technology company? 
-Are your current technology product marketing strategies falling short of your goals? Do you need some help improving your technology marketing plan?
-
-Then, keep reading! This article shares the best marketing strategies for IT companies and tech organizations so you can focus your time, energy, 
-and resources on the top technology marketing tactics.
-
-Why is technology marketing important?
-Every company needs marketing. It's the fuel behind attracting more traffic to your company's website, staying forefront of your customers' minds, 
-and generating new subscribers, customers and leads.
-
-Without Internet marketing, your potential customers may not even see your company in search results when they're looking for the exact products and services you offer.
-
-With effective technology marketing, a company like yours can reach more potential leads, educate those leads about the benefits your services 
-and create long-lasting customers. These customers can provide long-term revenue and profits for your business.
-
-Of course, for any company, the challenge is determining which technology product marketing strategies produce the best results.
-
-Online platforms have revolutionized the marketing industry and have proven to be quite the game changer for advertising products. However, 
-marketing tools are constantly evolving, so it is important for companies to always update their strategies by keeping up with the latest gadgets and software tools.
-
-To take the brand to the next level, a business should apply diverse marketing services that focus on different aspects. 
-Traditional methods focused on television and radio commercials as well as newspaper ads or simply putting marketing material in a mailbox and hoping for the best do not work anymore.
-
-Though advertising via television can still have some impact, there are numerous digital marketing strategies that can have a more direct and closer influence on the public.
-</p>
-
 </body>
 </html>
 <?php require_once('inc/footer.php'); ?>
