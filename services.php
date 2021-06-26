0270c3cee810b063a56190ae7c07068503c35d98
Srimal Fernando <dilankasrimal23@gmail.com>
2021-06-26 23:40:56 +0530

    Create services.php

---
+++ services.php
@@ -0,0 +1,62 @@
+<style type=text/css> .header {
+    background-image: linear-gradient(rgba(4,9,30,0.7),rgba(4,9,30,0.7)),url(img/about.gif);}
+</style>
+
+<?php $title = 'About Us - '; ?>
+<?php require_once('inc/header.php'); ?>
+  <div class="text-box">
+        <h1>Who We Are ?</h1>
+        <p>ShareTheMeal is a crowdfunding smartphone application to fight global hunger through the United Nations World Food Programme (WFP).</p>
+        <!-- <a href="" class="visit-btn">Visit us</a> -->
+        </div>
+    </section>
+<body>
+
+<h2>About Greenvibes Technologies</h2>
+<p>We are Greenvibes Technologies. A team with a diversified and forward thinking capabilities in the field of software with the elements of creating unique solutions.
+
+Just as machines have extended man's mechanical power and his convenience and comfort, Information Technology as commonly pictured by computers, 
+is extending man's mind or brain or intellectual power. 
+This paved the way for us to think out of the box and come up with systems spending our time and effort targeting areas that are untouched.
+
+advancing knowledge through research, and disseminating knowledge by means of the internet, researching and writing books, producing newsletters, editorials, monographs, seminars, lectures, conferences and the like.</p>
+
+
+<h2>Our Vision</h2>
+<p>Our Vision is to empower entrepreneurs with a roadmap and support that guides them through the transition from start-ups, to micro-enterprises, 
+and on to small and medium-sized businesses that create jobs, lead innovation, and increase economic development in their local communities.</p>
+
+<h2>Our Mission</h2>
+<p>Incorporating forward thinking with imagination, creativity, and originality to reach our goal to become a leader in software solutions.
+</p>
+
+<h2>Our Services</h2>
+<p>Have you hit a dead end with brainstorming marketing ideas for your technology company? 
+Are your current technology product marketing strategies falling short of your goals? Do you need some help improving your technology marketing plan?
+
+Then, keep reading! This article shares the best marketing strategies for IT companies and tech organizations so you can focus your time, energy, 
+and resources on the top technology marketing tactics.
+
+Why is technology marketing important?
+Every company needs marketing. It's the fuel behind attracting more traffic to your company's website, staying forefront of your customers' minds, 
+and generating new subscribers, customers and leads.
+
+Without Internet marketing, your potential customers may not even see your company in search results when they're looking for the exact products and services you offer.
+
+With effective technology marketing, a company like yours can reach more potential leads, educate those leads about the benefits your services 
+and create long-lasting customers. These customers can provide long-term revenue and profits for your business.
+
+Of course, for any company, the challenge is determining which technology product marketing strategies produce the best results.
+
+Online platforms have revolutionized the marketing industry and have proven to be quite the game changer for advertising products. However, 
+marketing tools are constantly evolving, so it is important for companies to always update their strategies by keeping up with the latest gadgets and software tools.
+
+To take the brand to the next level, a business should apply diverse marketing services that focus on different aspects. 
+Traditional methods focused on television and radio commercials as well as newspaper ads or simply putting marketing material in a mailbox and hoping for the best do not work anymore.
+
+Though advertising via television can still have some impact, there are numerous digital marketing strategies that can have a more direct and closer influence on the public.
+</p>
+
+</body>
+</html>
+<?php require_once('inc/footer.php'); ?>
