b0a4c70f0f48cabb79e0fbdf2334a889db25facc
Srimal Fernando <dilankasrimal23@gmail.com>
2021-06-27 23:24:23 +0530

    Update header.php
    for responsive

--- inc/header.php
+++ inc/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<title><?php echo $title; ?>Greenvibes</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
