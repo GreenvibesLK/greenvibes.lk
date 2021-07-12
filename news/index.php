00cb58ad62e6c6661c7cc98699c85f2ed5d4809a
Srimal Fernando <dilankasrimal23@gmail.com>
2021-07-12 23:54:56 +0530

    Update index.php
    css and onlick

--- news/index.php
+++ news/index.php
@@ -57,7 +57,7 @@
 </head>
 <body>
 <section class="navigation">
-  <h4>Greenvibes Technologies</h4>
+  <h4  onclick="function">Greenvibes Technologies</h4>
 
 </section>
 
@@ -82,6 +82,14 @@
 
 
 <?php require_once('inc/footer.php') ?>
+
+
+
+<script>
+  function URL() {
+    location.href = 'http://your.url.here';
+}
+</script>
 </body>
 </html>
 
