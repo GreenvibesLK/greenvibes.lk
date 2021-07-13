5b642ea994ed1edd29e84097538ede67f344465d
Srimal Fernando <dilankasrimal23@gmail.com>
2021-07-13 18:56:56 +0530

    footer and latest-post location changed
    main footer was merged

--- inc/footer.php
+++ inc/footer.php
@@ -1,23 +1,10 @@
-
-	<footer>
-        <h4>Greenvibes Technologies</h4>
-		<p>greenvibesLK 2021 copyright reserved</p>
-	</footer>
-	
-
-
-
-
-	<!-- responsive -->
-<script>
-    var navLinks = document.getElementById("navLinks");
-    
-    function showMenu(){
-        navLinks.style.right = "0";
-    }
-    function hideMenu(){
-        navLinks.style.right = "-200px";
-    }
-</script>
-</body>
-</html>
+<footer class="fnews">
+	<p>Copyright © 2021 Greenvibes Technologies
+	</p>
+  <ul>
+    <li><a href="https://fb.com/greenvibeslk"><i class="fab fa-facebook-f"></i></a></li>
+    <li><a href="https://twitter.com/greenvibeslk"><i class="fab fa-twitter"></i></a></li>
+    <li><a href="https://www.instagram.com/greenvibeslk/"><i class="fab fa-instagram"></i></a></li>
+    <li><a href="https://api.whatsapp.com/send/?phone=%2B94768623177&text&app_absent=0"><i class="fab fa-whatsapp"></i></a></li>
+  </ul>
+</footer>
