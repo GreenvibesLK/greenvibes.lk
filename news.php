f5a5dae2e708702e3de738b145f52bf5838d04bb
Srimal Fernando <dilankasrimal23@gmail.com>
2021-07-15 02:18:05 +0530

    change news logo to svg

--- news.php
+++ news.php
@@ -52,15 +52,14 @@
   <link rel="stylesheet" href="css/news.css">
   <link rel="stylesheet" href="fontawesome/css/all.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,800;1,900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,800;1,900&display=swap" rel="stylesheet">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>News - <?php echo $blog_title ?></title>
 </head>
 <body>
 <section class="navigation">
-  <a href="index.php"><i class="fas fa-home"></i></a>
-  <h4>Greenvibes Technologies</h4>
+  <a href="index.php"><img src="img/gv logo favicon white.svg" alt=""></a>
 </section>
 
 <div class="add">
@@ -101,5 +100,4 @@
 <div class="add addn">
   <p>Advertising area</p>
 </div><br><br>
-
 <?php require_once('inc/footer.php') ?>
