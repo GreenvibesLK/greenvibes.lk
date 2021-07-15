f50027feb0dededb0ceb47e1f58e39acff12dd1a
Srimal Fernando <dilankasrimal23@gmail.com>
2021-07-16 01:42:19 +0530

    custermize admin blog panel
    and latest news responsive for 1600px screens

--- admin/blog/index.php
+++ admin/blog/index.php
@@ -1,6 +1,6 @@
 <html>  
     <head>  
-        <title>Webslesson Demo - Live Table Add Edit Delete using Ajax Jquery in PHP Mysql</title>  
+        <title>Greenvibes - Admin</title>  
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
@@ -11,7 +11,7 @@
             <br />
 			<br />
 			<div class="table-responsive">  
-				<h3 align="center">Back to Tutorial - <a href="http://www.webslesson.info/2016/02/live-table-add-edit-delete-using-ajax-jquery-in-php-mysql.html" title="Live Table Add Edit Delete using Ajax Jquery in PHP Mysql">Live Table Add Edit Delete using Ajax Jquery in PHP Mysql</a></h3><br />
+				<h3 align="center">GV network blogs</h3><br />
 				<span id="result"></span>
 				<div id="live_data"></div>                 
 			</div>  
