016e2853f29c6403ed83c92f34749fd07fcefc7b
Srimal Fernando <dilankasrimal23@gmail.com>
2021-07-16 06:36:02 +0530

    erase date inserting
    becase i  did set the sql database for automatic time updating

--- admin/blog/insert.php
+++ admin/blog/insert.php
@@ -1,6 +1,7 @@
 <?php  
 require_once('inc/connection.php');
-$sql = "INSERT INTO {$table}(, blog_title, blog_short_title) VALUES('".$_POST["blog_title"]."', '".$_POST["blog_short_title"]."')";  
+$blog_short_title = substr($_POST["blog_title"] , 0,100);
+$sql = "INSERT INTO {$table}( blog_title, blog_short_title) VALUES('".$_POST["blog_title"]."', '".$blog_short_title."')";  
 if(mysqli_query($connect, $sql))  
 {  
      echo 'Data Inserted';  
