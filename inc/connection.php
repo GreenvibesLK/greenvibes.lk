ac1803dd4dec398df977b8f132414521197749ad
Srimal Fernando <dilankasrimal23@gmail.com>
2021-07-16 21:24:18 +0530

    Update connection.php
    clear server info

--- inc/connection.php
+++ inc/connection.php
@@ -1,12 +1,4 @@
-<!-- <?php 
-	$server = "localhost";
-	$user = "greenvi_function";
-    $pass = "%,pxPI(}awU[";
-    $dbname = "greenvi_blog";
-    $connection = mysqli_connect($server, $user, $pass, $dbname);
-    $table = "blog";
- ?> -->
- <?php 
+<?php 
  	$server = "localhost";
 	$user = "root";
     $pass = "";
