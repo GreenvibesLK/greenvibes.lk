01d267f2509014e19d0054056f1c7b2dff44e1ad
Srimal Fernando <dilankasrimal23@gmail.com>
2021-07-16 17:32:11 +0530

    Create  header
    inc folder

---
+++ admin/inc/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Log In - User Management System</title>
+	<link rel="stylesheet" href="css/main.css">
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body class="content">
--- admin/index.php
+++ admin/index.php
@@ -56,19 +56,8 @@
 		}
 	}
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Log In - User Management System</title>
-	<link rel="stylesheet" href="css/main.css">
-	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-</head>
-<body class="content">
-	<div class="container">
+<?php require_once('inc/header.php'); ?>
+<div class="container">
         <div class="row justify-content-center align-items-center" style="height:100vh">
             <div class="col-4">
               <h3 align="center">Admin Login</h3>
