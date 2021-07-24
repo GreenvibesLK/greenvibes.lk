4d3e5c66dfb7bf53ea086a54a425f12d07a50d0f
Srimal Fernando <dilankasrimal23@gmail.com>
2021-07-25 04:44:14 +0530

    admin date auto update

--- admin/insert.php
+++ admin/insert.php
@@ -1,7 +1,10 @@
 <?php  
 require_once('inc/connect.php');
 $blog_short_title = substr($_POST["blog_title"] , 0,100);
-$sql = "INSERT INTO {$table}( blog_title, blog_short_title, online) VALUES('".$_POST["blog_title"]."', '".$blog_short_title."', '".$_POST["online"]."')";  
+$date = date("Y-m-d");
+$sql = "INSERT INTO {$table}( blog_title, blog_short_title, online, blog_date) VALUES('".$_POST["blog_title"]."', '".$blog_short_title."', false, '{$date}' )"; 
+
+echo $sql;
 if(mysqli_query($connect, $sql))  
 {  
      echo 'Data Inserted';  
