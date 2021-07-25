6fdad98aaa4f73beac1eab05e60953c6b721bba7
Srimal Fernando <dilankasrimal23@gmail.com>
2021-07-25 12:25:17 +0530

    Update 404.php
    custom 404 page

---
+++ 404.php
@@ -0,0 +1,12 @@
+<style type=text/css> .header {
+    background-image: linear-gradient(rgba(4,9,30,0.7),rgba(4,9,30,0.7)),url(img/about.gif);}
+</style>
+
+<?php $title = 'Services - '; ?>
+<?php require_once('inc/header.php'); ?>
+  <div class="text-box">
+        <h1>404 ERROR</h1>
+        <p>This page not founded.</p>
+        <!-- <a href="" class="visit-btn">Visit us</a> -->
+        </div>
+    </section>
